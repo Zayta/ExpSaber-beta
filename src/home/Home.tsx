db7f7cc4d6a6f691ef8b7d2e37c417d4b4f439a8
Sandra Zhen <szhen8930@gmail.com>
2021-07-24 17:13:48 -0400

    attempt to use react class and context failed

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -5,26 +5,40 @@ import { PlayerContext, PlayerContextData} from './data/context/PlayerContext'
 import usePlayerContextValue from './data/api/Api';
 import Search from './search/Search';
 
-//uses context data, makes sure values are not undefined or null
-const Home = ()=> {
-  const [searched,setSearched] = useState(false);
-  const [ssid, setSsid] = useState('76561198810679866');
 
-  const search = function(ss_id:string){
-    setSsid(ss_id);
-    setSearched(true);
+
+export default class Home extends React.Component{
+  state: HomeState = {
+    ssid: '',
+    searched:false
+  };
+
+
+  search = (ss_id:string)=>{
+    this.setState({
+      ssid:ss_id,
+      searched:true
+    });
     console.log('set ssid to '+ss_id)
+    console.log('set ssid to '+this.state.ssid)
   }
-    let playerContextValue = usePlayerContextValue(ssid);
+  
+  render(){
+    
+    let playerContextValue = usePlayerContextValue(this.state.ssid);
     return  <div>
-      <Search setScoreSaberID={search}/>
+      <Search setScoreSaberID={this.search}/>
       {
-        searched?
+        this.state.searched?
         <PlayerContext.Provider value={playerContextValue} >
-          <Main ssid={ssid}/>
+          <Main ssid={this.state.ssid}/>
         </PlayerContext.Provider>:<div></div>
       }
     </div>
+  }
+}
+interface HomeState{
+  ssid:string,
+  searched:boolean
 }
 
-export default Home;
