b9007ea4c0ff55a6886149c4dfc21ce37035bbdd
Sandra Zhen <szhen8930@gmail.com>
2021-08-18 16:01:59 -0400

    changed loading color

--- src/common/Loading.tsx
+++ src/common/Loading.tsx
@@ -3,7 +3,7 @@ import Loader from 'react-loader-spinner';
 const LoadingIndicator = () => {
     const { promiseInProgress } = usePromiseTracker();
     if(promiseInProgress){
-        return <div><Loader type = "ThreeDots"/></div>
+        return <div><Loader type = "ThreeDots" color="#ffffff"/></div>
     }
     else{
         return <div></div>
