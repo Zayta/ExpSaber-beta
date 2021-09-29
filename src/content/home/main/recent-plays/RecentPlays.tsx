9539c5e93dca0d34bb12804b46e1a61f6cbfeee0
Sandra Zhen <szhen8930@gmail.com>
2021-09-29 12:04:40 -0400

    implemented filter feat

--- src/content/home/main/recent-plays/RecentPlays.tsx
+++ src/content/home/main/recent-plays/RecentPlays.tsx
@@ -1,15 +1,42 @@
-import ScoresData from "../../data/models/ScoreData";
+import { useState } from "react";
+import styled from "styled-components";
+import ScoresData, { Score } from "../../data/models/ScoreData";
 import PlaysLi from "./PlaysLi";
+const FilterContainer = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:flex-end;
+`;
 
 const RecentPlays = (props:RecentPlaysProps) =>{
+    //set filter to empty initially
+    const [filter, setFilter] = useState('');
+    //condition for filter (by songName, song author etc. modify this to include dif criteria)
+    function filterCondition(s:Score){
+        const filterLC = filter.toLowerCase();
+        return s.songName.toLowerCase().includes(filterLC)
+        || s.songSubName.toLowerCase().includes(filterLC)
+        || s.songAuthorName.toLowerCase().includes(filterLC)
+        || s.levelAuthorName.toLowerCase().includes(filterLC)
+        ||filter=='';
+    }
+
     if(!props.scoresData){
         return <div>No recent plays</div>
     }
-
     return <div>
+          <FilterContainer>
+            <input id="filter"
+            name="filter"
+            type="text"
+            placeholder="Filter"            
+            value={filter}
+            onChange={event => setFilter(event.target.value)}
+            />
+            </FilterContainer>
         <ul>
         {
-            props.scoresData.scores.map(score =><PlaysLi key = {score.scoreId} score = {score}/>)
+            props.scoresData.scores.filter(filterCondition).map(score =><PlaysLi key = {score.scoreId} score = {score}/>)
         }
         </ul>
         </div>
