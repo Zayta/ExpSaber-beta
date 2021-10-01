4c3f5734831d5cd900a6d3fec3c7675a0a36e1fb
Sandra Zhen <szhen8930@gmail.com>
2021-10-01 05:50:29 -0400

    refactor to search context

--- src/content/home/Home.tsx
+++ src/content/home/Home.tsx
@@ -55,15 +55,10 @@ const HomeContent =  () => {
   const params = useParams<HomeParams>();//grab params from url
   let ssPlayerData:PlayerData|undefined = useSSPlayerData(params.ssid);
   
-  const [sortOrder,setSortOrder] = useState(ScoreSortOrder.RECENT);
-  const [pages,setPages] = useState(3);
-  console.log('set sort order at home is ')
-  console.log(setSortOrder);
-  
-  let ssScoresData:Score[]|undefined = useScoresData(params.ssid,sortOrder,pages);
+  let ssScoresData:Score[]|undefined = useScoresData(params.ssid,ScoreSortOrder.RECENT,3);
   return (
       <div>
-        <Search setSortOrder={setSortOrder}/>
+        <Search/>
         <HomeContainer>
         {
           ssPlayerData?
---
+++ src/content/home/search/SearchContext.tsx
@@ -0,0 +1,9 @@
+
+const SearchContext = ()=>{
+    // const [sortOrder,setSortOrder] = useState(ScoreSortOrder.RECENT);
+    // const [pages,setPages] = useState(3);
+    // console.log('set sort order at home is ')
+    // console.log(setSortOrder);
+}
+
+export default SearchContext;
